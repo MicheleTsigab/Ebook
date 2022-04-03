833136a54e95f9866e1c1bddd5c926f5f7476b43
Michele <micheletsigab@gmail.com>
2022-04-03 19:08:04 +0300

    added auto now field true

--- ebook_api/models.py
+++ ebook_api/models.py
@@ -10,7 +10,7 @@ class author(models.Model):
 class ebook(models.Model):
     cover=models.ImageField()
     title=models.CharField(max_length=100)
-    year=models.DateField()
+    year=models.DateField(auto_now_add=True)
     price=models.FloatEbookApiConfigField() 
     path=models.FileField() 
     lang=models.CharField(max_length=30) 
@@ -18,20 +18,19 @@ class ebook(models.Model):
 class ebook_authored(models.Model):
     ebook=models.ForeignKey(ebook,on_delete=models.CASCADE)
     author=models.ForeignKey(author,on_delete=models.CASCADE)
-    date=models.DateField()
+    date=models.DateField(auto_now_add=True)
 class ebook_bought(models.Model):
     ebook=models.ForeignKey(ebook)
     customer=models.ForeignKey(customer,on_delete=models.CASCADE)
-    date=models.DateField()
+    date=models.DateField(auto_now_add=True)
 class genre(models.Model):
     genre=models.CharField(max_length=50)
     sub_genre=models.CharField(max_length=50)
-
 class reviews(models.Model):
     customer=models.ForeignKey(customer,on_delete=models.CASCADE)
     ebook=models.ForeignKey(ebook,on_delete=models.CASCADE)
     rate=models.SmallIntegerField()
-    date=models.DateField()
+    date=models.DateField(auto_now_add=True)
     reviews=models.TextField()
 class discounts(models.Model):
     discount=models.FloatField()
