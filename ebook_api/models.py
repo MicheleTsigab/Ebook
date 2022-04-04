1e56dc9cd2fc1ede717162835cf600b793103bc0
Michele <micheletsigab@gmail.com>
2022-04-04 15:46:26 +0300

    string rep and validator in models

--- ebook_api/models.py
+++ ebook_api/models.py
@@ -1,14 +1,20 @@
-#from cgi import print_exception
-#from distutils.command.config import LANG_EXT
-#from turtle import title
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 class customer(models.Model):
-    customer=models.OneToOneField(User,on_delete=models.CASCADE)  
+    customer=models.OneToOneField(User,on_delete=models.CASCADE) 
+    bought_ebooks=models.ManyToManyField('ebook',through='bought_ebook') 
+    def __str__(self):
+        return self.customer.username
+    
 class author(models.Model):
-    customer=models.OneToOneField(User,on_delete=models.CASCADE)
+    author=models.OneToOneField(User,on_delete=models.CASCADE)
+    authored_ebooks=models.ManyToManyField('ebook',through='authored_ebook')
+    def __str__(self):
+        return self.author.username
 class ebook(models.Model):
-    cover=models.ImageField()
+    cover=models.ImageField(blank=True,null=True)
     title=models.CharField(max_length=100)
     year=models.DateField(auto_now_add=True)
     price=models.FloatField() 
@@ -17,23 +23,33 @@ class ebook(models.Model):
     genre=models.ManyToManyField('genre')
     def __str__(self):
         return self.title
-class ebook_authored(models.Model):
+class authored_ebook(models.Model):
     ebook=models.ForeignKey(ebook,on_delete=models.CASCADE)
     author=models.ForeignKey(author,on_delete=models.CASCADE)
     date=models.DateField(auto_now_add=True)
-class ebook_bought(models.Model):
+    def __str__(self):
+        return self.ebook.title + " by " + self.author.author.username
+class bought_ebook(models.Model):
     ebook=models.ForeignKey(ebook,on_delete=models.CASCADE)
     customer=models.ForeignKey(customer,on_delete=models.CASCADE)
     date=models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.ebook.title
 class genre(models.Model):
     genre=models.CharField(max_length=50)
     sub_genre=models.CharField(max_length=50)
+    def __str__(self):
+        return self.genre
 class reviews(models.Model):
     customer=models.ForeignKey(customer,on_delete=models.CASCADE)
     ebook=models.ForeignKey(ebook,on_delete=models.CASCADE)
-    rate=models.SmallIntegerField()
+    rate=models.SmallIntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
     date=models.DateField(auto_now_add=True)
-    reviews=models.TextField()
-class discounts(models.Model):
+    review=models.TextField()
+    def __str__(self):
+        return self.review
+class discount(models.Model):
     discount=models.FloatField()
+    def __str__(self):
+        return str(self.discount)+"%"
 
