eb77284c077bb7e931d487bba2c9d99f8ef3856f
Michele <micheletsigab@gmail.com>
2022-04-05 12:00:09 +0300

    registered all models to admin

--- ebook_api/admin.py
+++ ebook_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import author, genre, ebook, authored_ebook,bought_ebook,review,discount,customer
+
+admin.site.register(ebook)
+admin.site.register(author)
+admin.site.register(customer)
+admin.site.register(authored_ebook)
+admin.site.register(bought_ebook)
+admin.site.register(review)
+admin.site.register(discount)
+admin.site.register(genre)
