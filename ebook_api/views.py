ef784d3ce2cd900240bbb597d0ba56f18fc688df
Michele <micheletsigab@gmail.com>
2022-04-05 12:00:50 +0300

    trial view and authorviewsets

--- ebook_api/views.py
+++ ebook_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import *
+from .serializers import authorserializer, ebookserializer
+@api_view()
+def first_api_view(request):
+    ebooks=ebook.objects.all()
+    ebook_serializers=ebookserializer(ebooks,many=True)
+    return Response(ebook_serializers.data)
+class authorViewset(viewsets.ModelViewSet):
+    serializer_class = authorserializer
+    queryset = author.objects.all()
